a227b82faed569cb5b8e00bee659a5c35c0ddab9
Hitesh <hitesh.gupta2507@yahoo.in>
2018-11-17 13:56:56 -0500

    Movie Recommendation System

---
+++ Recommender_System/recommendation_system.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[24]:
+
+
+import argparse
+import json
+import numpy as np
+
+from compute_similarity_score import pearson_score
+from collaborative_filtering_score import *
+
+# Define a function to parse the input arguments. The only input argument would be the name of the user:
+def build_arg_parser():
+    parser = argparse.ArgumentParser(description = "Find the movie recommendations for the given user")
+    parser.add_argument('--user', dest = "user", required = True, help = "Input user")
+    return parser
+
+
+# In[31]:
+
+
+# Get movie recommendations for the input user
+def get_recommendations(dataset, input_user):
+    if input_user not in dataset:
+        raise TypeError('Cannot find ' + input_user + ' in the dataset')
+
+    overall_scores = {}
+    similarity_scores = {}
+
+    for user in [x for x in dataset if x != input_user]:
+        similarity_score = pearson_score(dataset, input_user, user)
+
+        if similarity_score <= 0:
+            continue
+        
+        filtered_list = [x for x in dataset[user] if x not in \
+                dataset[input_user] or dataset[input_user][x] == 0]
+
+        for item in filtered_list: 
+            overall_scores.update({item: dataset[user][item] * similarity_score})
+            similarity_scores.update({item: similarity_score})
+
+    if len(overall_scores) == 0:
+        return ['No recommendations possible']
+
+    # Generate movie ranks by normalization 
+    movie_scores = np.array([[score/similarity_scores[item], item] 
+            for item, score in overall_scores.items()])
+
+    # Sort in decreasing order 
+    movie_scores = movie_scores[np.argsort(movie_scores[:, 0])[::-1]]
+
+    # Extract the movie recommendations
+    movie_recommendations = [movie for _, movie in movie_scores]
+
+    return movie_recommendations
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    args = build_arg_parser().parse_args()
+    user = args.user
+    
+    ratings_file = 'ratings.json'
+    
+    with open(ratings_file, 'r') as f:
+        data = json.loads(f.read())
+        
+    print("\n Movie recommendations for " + user + ":")
+    movies = get_recommendations(data, user)
+    for i, movie in enumerate(movies):
+        print(str(i+1) + '. ' + movie)
+
